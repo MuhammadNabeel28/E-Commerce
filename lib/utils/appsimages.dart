0d2667a9df534acba2b369d6b8fe4cfab8e6e2b9
MuhammadNabeel28 <nabeelmansoori28@gmail.com>
2023-09-13 03:54:04 +0500

    change File Text

--- lib/utils/appsimages.dart
+++ lib/utils/appsimages.dart
@@ -6,4 +6,10 @@ class AppsImages {
   static const String arrow = 'assets/appassets/Arrow 4.png';
   static const String bottomHome = 'assets/appassets/home.png';
   static const String bottomHomeInactive = 'assets/appassets/home_white.png';
+  static const String serchIcon = 'assets/appassets/SearchIcon.png';
+}
+
+class AppsSvgIcon {
+  static const String serchIcon = 'assets/appassets/SearchIcon.svg';
+  static const String cardIcon = 'assets/appassets/bag.svg';
 }
--- lib/view/home/homeview.dart
+++ lib/view/home/homeview.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/constants/apps_color.dart';
+import 'package:flutter_ecommerce/utils/appsimages.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,11 +11,60 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 252,
+                width: MediaQuery.of(context).size.width * 10,
+                color: AppsColors.blue,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width * 0.1,
+                            bottom: 52,
+                          ),
+                          child: const Text(
+                            'Hey, Halal',
+                            style: TextStyle(
+                              fontFamily: 'Manrope',
+                              fontWeight: FontWeight.w700,
+                              fontSize: 22,
+                              color: AppsColors.white,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.width * 0.5,
+                            bottom: 52,
+                          ),
+                          child: SvgPicture.asset(
+                            AppsSvgIcon.cardIcon,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    // SvgPicture.asset(
+                    //   AppsSvgIcon.serchIcon,
+                    // ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
